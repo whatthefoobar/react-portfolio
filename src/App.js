8573bb67d38eacd534488a6b5606b3162f95f4d3
whatthefoobar <irina.mntn@gmail.com>
2022-05-11 15:59:43 +0200

    added header but not functional yet

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
+// import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Education from './components/Education/Education';
 import Portfolio from './components/Portfolio/Portfolio';
 import Contact from './components/Contact/Contact';
+import { useState } from 'react';
 
 function App() {
+  const [sidebar, setSidebar] = useState(true);
+
+  const showSidebar = () => setSidebar(!sidebar);
+  //if sidebar then add active class to body and header and toggle the icon of the #menu-btn
   return (
-    <div className="App">
+    <div className={sidebar ? 'App active' : 'App'}>
       <Router>
         <Header />
+        {/* <Navbar /> */}
         <Routes>
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaBars } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaMailBulk } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [sidebar, setSidebar] = useState(false);
   return (
-    <header class="header">
-      <FaBars id="menu-btn" fill="white" />
-
-      <a href="#home" class="logo">
+    <header class="header active">
+      {/* add a n active class here to toggle */}
+      {sidebar || <FaBars id="menu-btn" fill="white" />}
+      <Link to="/" class="logo ">
         IMB
-      </a>
-      <nav class="navbar">
-        <Link to="/" class="active">
+      </Link>
+
+      <nav class="navbar ">
+        <Link to="/" class="link active">
           Home
         </Link>
-        <Link to="/about">About</Link>
-        <Link to="/education">Education</Link>
-        <Link to="/portfolio">Portfolio</Link>
-        <Link to="/contact">Contact</Link>
-        {/* <a href="#home" class="active">
-          Home
-        </a>
-        <a href="#about">About</a>
-        <a href="#portfolio">Portfolio</a>
-        <a href="#contact">Contact</a> */}
+        <Link to="/about" className="link">
+          About
+        </Link>
+        <Link to="/education" className="link">
+          Education
+        </Link>
+        <Link to="/portfolio" className="link">
+          Portfolio
+        </Link>
+        <Link to="/contact" className="link">
+          Contact
+        </Link>
       </nav>
       <div class="follow">
+        <a href="https://github.com/whatthefoobar">
+          <FaGithub fill="#000" className="social-icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/irina-munteanu-b%C3%A4ck-523a41b4/">
+          <FaLinkedin fill="#000" className="social-icon" />
+        </a>
+        <a href="mailto:irina.mntn@gmail.com">
+          <FaMailBulk fill="#000" className="social-icon" />
+        </a>
+
         {/* <a href="#" class="fab fa-github"></a>
           <a href="#" class="fab fa-linkedin"></a>
           <a href="#" class="fab fa-instagram"></a> */}
